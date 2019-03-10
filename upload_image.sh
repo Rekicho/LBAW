d88658ca4b2dcd7bbee52ae3f7d3944928786602
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-03-10 12:48:50 +0000

    Updated upload_image.sh for a3 delivery

--- upload_image.sh
+++ upload_image.sh
@@ -4,7 +4,7 @@
 set -e
 
 DOCKER_USERNAME=mrzephyr17 # Replace by your docker hub username
-IMAGE_NAME=lbaw1844
+IMAGE_NAME=lbaw1844-piu
 
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
