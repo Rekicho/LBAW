99f96ca0dd2ba3da342b1aa2c6376d24a717a971
Simao Silva <up201605422@fe.up.pt>
2019-03-17 18:07:57 +0000

    corrected responsive modal

--- html/back-office/script.js
+++ html/back-office/script.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
+
     $(window).on("resize", function (e) {
         checkScreenSize();
     });
 
+    const MAX_WIDTH = 992;
+
+    let isModalEnabled = false;
+    let productsTableLines = document.querySelectorAll("#productsTable > tr");
+
     checkScreenSize();
 
-    function checkScreenSize(){
-        var newWindowWidth = $(window).width();
-        const maxWidth = 481;
-        if (newWindowWidth < maxWidth) {
-
-            $('#products').on("click", '#productsTable tr', function(){
-                $('#actionsModal').modal('toggle');
+    function checkScreenSize() {
+        let newWindowWidth = $(window).width();
+        if (!isModalEnabled && newWindowWidth < MAX_WIDTH) {
+            productsTableLines.forEach(function(line) {
+                line.setAttribute("data-toggle", "modal");
+                line.setAttribute("data-target", "#actionsModal");
             });
+            isModalEnabled = true;
+        }
+        else if (isModalEnabled && newWindowWidth >= MAX_WIDTH) {
+            productsTableLines.forEach(function(line) {
+                line.removeAttribute("data-toggle");
+                line.removeAttribute("data-target");
+            });
+            isModalEnabled = false;
         }
     }
 });
