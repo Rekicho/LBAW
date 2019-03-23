eba18f6724592931bc8095b8fa7488cf186afd11
Bruno Miguel da Silva Barbosa Sousa <up201604145@fe.up.pt>
2019-03-23 15:07:44 +0000

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,6 @@
 -- Types
 
-CREATE TYPE purchase_state AS ENUM ('Waiting for payment', 'Waiting for payment approval', 'Paid', 'Shipped', 'Completed');
+CREATE TYPE state_purchase AS ENUM ('Waiting for payment', 'Waiting for payment approval', 'Paid', 'Shipped', 'Completed');
 
 --Tables
 
@@ -27,14 +27,14 @@ CREATE TABLE user (
 );
 
 CREATE TABLE client (
-    id INTEGER REFERENCES user (id) PRIMARY KEY,
+    id INTEGER REFERENCES user (id) PRIMARY KEY ON UPDATE CASCADE,
     username TEXT NOT NULL CONSTRAINT client_username_uk UNIQUE,
     email TEXT NOT NULL CONSTRAINT client_email_uk UNIQUE,
     password TEXT NOT NULL
 );
 
 CREATE TABLE staff_member (
-    id INTEGER REFERENCES user (id) PRIMARY KEY,
+    id INTEGER REFERENCES user (id) PRIMARY KEY ON UPDATE CASCADE,
     username TEXT NOT NULL CONSTRAINT client_username_uk UNIQUE,
     password TEXT NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE NOT NULL,
@@ -42,14 +42,87 @@ CREATE TABLE staff_member (
 );
 
 CREATE TABLE wishlist (
-    id_product INTEGER NOT NULL REFERENCES product (id),
-    id_client INTEGER NOT NULL REFERENCES client (id),
+    id_product INTEGER NOT NULL REFERENCES product (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE,
     PRIMARY KEY (id_product, id_client)
 );
 
 CREATE TABLE cart (
-    id_product INTEGER NOT NULL REFERENCES product (id),
-    id_client INTEGER NOT NULL REFERENCES client (id),
+    id_product INTEGER NOT NULL REFERENCES product (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE,
     quantity INTEGER NOT NULL CONSTRAINT quantity_ck CHECK (quantity >= 0),
     PRIMARY KEY (id_product, id_client)
+);
+
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
+    id_product INTEGER NOT NULL REFERENCES product (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE,
+    comment TEXT NOT NULL,
+    rating INTEGER NOT NULL CONSTRAINT review_rating_ck CHECK (((rating > 0) OR (rating <= 5))),
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    CONSTRAINT review_product_client_uk UNIQUE(id_product, id_client)
+);
+
+CREATE TABLE report (
+    id SERIAL PRIMARY KEY,
+    reason TEXT NOT NULL,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    id_review INTEGER NOT NULL REFERENCES review (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE report_treatment (
+    report_id INTEGER REFERENCES report (id) PRIMARY KEY ON UPDATE CASCADE,
+    id_staff_member INTEGER NOT NULL REFERENCES staff_member (id) ON UPDATE CASCADE,
+    has_deleted BOOLEAN NOT NULL,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE billing_information (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    zip_code TEXT NOT NULL,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE purchase (
+    id SERIAL PRIMARY KEY,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    id_billing_information INTEGER NOT NULL REFERENCES billing_information (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE purchase_state (
+    id SERIAL PRIMARY KEY,
+    TYPE state_purchase CONSTRAINT state_purchase_uk UNIQUE NOT NULL
+);
+
+CREATE TABLE purchased_product (
+    id_product INTEGER NOT NULL REFERENCES product (id) ON UPDATE CASCADE,
+    id_purchase INTEGER NOT NULL REFERENCES purchase (id) ON UPDATE CASCADE,
+    name TEXT NOT NULL,
+    price FLOAT NOT NULL CONSTRAINT purchased_product_price_ck CHECK (price >= 0),
+    description TEXT NOT NULL,
+    discount FLOAT NOT NULL CONSTRAINT purchased_product_discount_ck CHECK (((discount >= 0) OR (discount <= 1))),
+    quantity INTEGER NOT NULL CONSTRAINT purchased_product_quantity_ck CHECK (quantity >= 0),
+    PRIMARY KEY (id_product, id_purchase)
+);
+
+CREATE TABLE purchase_transition (
+    id SERIAL PRIMARY KEY,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    id_purchase_state INTEGER NOT NULL REFERENCES purchase_state (id) ON UPDATE CASCADE,
+    id_purchase INTEGER NOT NULL REFERENCES purchase (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE toggle_active (
+    id_staff_member INTEGER NOT NULL REFERENCES staff_member (id) ON UPDATE CASCADE,
+    id_client INTEGER NOT NULL REFERENCES client (id) ON UPDATE CASCADE,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    is_ban BOOLEAN NOT NULL,
+    PRIMARY KEY (id_staff_member, id_client)
 );
