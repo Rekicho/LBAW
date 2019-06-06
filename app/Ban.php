dfded4bf2a5a16a9a6c63b3539ba94c33bac0547
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-06-06 16:27:53 +0100

    Ban working

--- app/Ban.php
+++ app/Ban.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Ban extends Model
 {
-    public $timestamps  = false;
+    public $timestamps = false;
+    protected $table = 'bans';
 
     public static function getLastBan($user){
         return Ban::where('id_client', $user->id)->orderBy('start_t', 'desc')->first();
